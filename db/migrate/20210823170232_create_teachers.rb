a4367128cd22ee1c58dead065e17623a91c664f4
Henry Mutschler <henryamutschler@gmail.com>
2021-08-24 11:33:13 +0100

    Migration works now :))

--- db/migrate/20210823170232_create_teachers.rb
+++ db/migrate/20210823170232_create_teachers.rb
@@ -3,7 +3,7 @@ class CreateTeachers < ActiveRecord::Migration[6.0]
     create_table :teachers do |t|
       t.string :category
       t.text :description
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
