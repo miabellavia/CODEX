de2af0868bd97ddb24ce9124654ac879fee62386
Ivory Lau <ivory99142@gmail.com>
2021-08-24 17:32:06 +0100

    created booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,30 +1,48 @@
 class BookingsController < ApplicationController
-  # before_action :set_user, only: [:create, :show]
+  before_action :set_user, only: [:index, :show, :edit, :destroy]
 
-  # def new
-  #   @booking = Booking.new
-  # end
+  def index
+    @bookings = Booking.where(user: @user)
+  end
 
-  # def create
-  #   @booking = Booking.new(booking_params)
-  #   if @booking.save
-  #     redirect_to booking_path(@booking)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def show
+    @booking = Booking.find(params[:id])
+  end
 
-  # def show
-  #   @bookings = @user.bookings
-  # end
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path(@user)
+  end
 
-  # private
+  def new
+    @booking = Booking.new
+  end
 
-  # def set_user
-  #   @user = User.find(params[:user_id]
-  # end
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking), notice: 'You created a new Booking!'
+    else
+      render :new
+    end
+  end
 
-  # def booking_params
-  #   params.require(:booking).permit(:start_time, :end_time, :teacher_id)
-  # end
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking), notice: 'The booking is updated.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id]
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :teacher_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   resources :teachers do
     get '/dashboard', to: 'teachers#dashboard'
+    resources :bookings, only: [:new, :create]
   end
+
+  resources :bookings, only: [:index, :show, :update, :destroy]
 end
