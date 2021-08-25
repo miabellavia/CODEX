f78fcad40e49f161cef8d4340548a965dd080852
Henry Mutschler <henryamutschler@gmail.com>
2021-08-25 15:02:38 +0100

    made the seeds for teachers and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+15.times do
+    user = User.new(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      username: Faker::Name.first_name,
+      email: Faker::Internet.email,
+      password: "123456"
+    )
+    user.save
+end
+    
+3.times do
+      teacher = Teacher.new(
+        user_id: rand(1..15),
+        description: Faker::Lorem.sentence,
+        category: ["Front-End Specialist", "Back-End Specialist", "Full Stack Specialist"].sample
+    )
+    teacher.save
+  end
